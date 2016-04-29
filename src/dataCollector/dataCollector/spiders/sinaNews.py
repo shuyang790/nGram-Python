2a02c35f4d893ed1f9b22de2a5f692a6e7c96235
Shuyang Shi <shuyang790@gmail.com>
2016-04-29 19:16:52 +0800

    update dataCollector-spider: new source

--- src/dataCollector/dataCollector/spiders/sinaNews.py
+++ src/dataCollector/dataCollector/spiders/sinaNews.py
@@ -8,15 +8,16 @@ sys.setdefaultencoding("utf-8")
 
 year = 2004
 base_directory = "../../data/" + str(year) + "/"
-base_url = "http://news.sina.com.cn/hotnews/"
+base_url = "http://news.sina.com.cn/old1000/news1000_"
 
 months = [x for x in range(1, 13)]
 days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-class sinaNews0405Spider(scrapy.Spider):
-    name = "sinaNews0405"
+class sinaNewsSpider(scrapy.Spider):
+    name = "sinaNews"
     allowed_domains = ['sina.com.cn']
-    start_urls = []
+    start_urls = [
+    ]
 
     for year in range(year, year+1):
         for month in months:
@@ -27,21 +28,24 @@ class sinaNews0405Spider(scrapy.Spider):
     def news_parse(self, response):
         item = response.meta['item']
 
+        title = response.xpath('//*[contains(concat(" ", normalize-space(@class), " "), " f24 ")]')\
+            .xpath('font/text()').extract()[0]
+
         for sel in response.xpath('//*[contains(concat(" ", normalize-space(@id), " "), " zoom ")]'):
             text = ""
             for psg in sel.xpath('p'):
                 if len(psg.xpath('text()')) > 0:
                     text += psg.xpath('text()').extract()[0]
-            psg = {'title': item['title'], 'text': text}
+            psg = {'title': title, 'text': text}
         os.system("mkdir -p \"" + base_directory + '/'.join(response.url.split('/')[2:-1]) + "\"")
         filename = base_directory + '/'.join(response.url.split('/')[2:])
         filename = '.'.join(filename.split('.')[:-1] + ["txt"])
         with open(filename, "w") as f:
-            f.write(psg['title'] + '\n')
-            f.write(psg['text'] + '\n')
+            f.write(psg['title'].encode("utf-8") + '\n')
+            f.write(psg['text'].encode("utf-8") + '\n')
 
     def parse(self, response):
-        for sel in response.xpath('//*[contains(concat(" ", normalize-space(@class), " "), " a01 ")]'):
+        for sel in response.xpath('//ul/ul/ul/li/a'):
             if sel.xpath('@href').extract() == [] or sel.xpath('text()').extract() == []:
                 continue
             item = DatacollectorItem()
